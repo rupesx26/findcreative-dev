87f8ae2226dc0d108be21fc4041ea7f07e11849d
Rupesh Mayekar <rupesh.mayekar@bookmyshow.com>
2020-01-25 15:45:27 +0530

    update changes

--- scripts/startProd.js
+++ scripts/startProd.js
@@ -27,7 +27,9 @@ if (cluster.isMaster) {
     }
 
     console.info(
-      `Server running on port ${PORT} -- Worker pid: ${cluster.worker.process.pid}`
+      `Server running on port ${PORT} -- Worker pid: ${
+        cluster.worker.process.pid
+      }`
     );
   });
 }
